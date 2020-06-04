f7a05c76a90492a35df5bad38107a3012132d5f5
suzheng <593012736@qq.com>
2020-06-04 20:39:57 +0800

    项目搭建

---
+++ README.md
@@ -0,0 +1,7 @@
+## 摆地摊小程序
+
+coolwell-booth-ui
+
+## npm install
+
+## npm run dev:weapp
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-quotes */
+import Taro, { Component } from "@tarojs/taro";
+import { View, Button, Text } from "@tarojs/components";
+import { observer, inject } from "@tarojs/mobx";
+
+import "./index.scss";
+
+@inject("counterStore")
+@observer
+class Index extends Component {
+  // eslint-disable-next-line react/sort-comp
+  config = {
+    navigationBarTitleText: "首页"
+  };
+
+  componentWillMount() {}
+
+  componentWillReact() {
+    console.log("componentWillReact");
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  increment = () => {
+    const { counterStore } = this.props;
+    counterStore.increment();
+  };
+
+  decrement = () => {
+    const { counterStore } = this.props;
+    counterStore.decrement();
+  };
+
+  incrementAsync = () => {
+    const { counterStore } = this.props;
+    counterStore.incrementAsync();
+  };
+
+  render() {
+    const {
+      counterStore: { counter }
+    } = this.props;
+    return (
+      <View className="index">
+        <Button onClick={this.increment}>+</Button>
+        <Button onClick={this.decrement}>-</Button>
+        <Button onClick={this.incrementAsync}>Add Async</Button>
+        <Text>{counter}</Text>
+      </View>
+    );
+  }
+}
+
+export default Index;
